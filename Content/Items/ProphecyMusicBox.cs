e93cca8107a6647cfb4d8bcfe1c953db8ca6dcce
Darian Hughes <darianmhughes7@gmail.com>
2025-08-18 02:00:40 -0500

    moved the yharim music box over to this project from infernal eclipse

---
+++ Content/Items/ProphecyMusicBox.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using InfernumMode.Content.Items.Relics;
+using YharimEX.Content.Tilles;
+
+namespace YharimEX.Content.Items
+{
+    public class ProphecyMusicBox : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            if (Main.dedServ)
+                return;
+            Item.ResearchUnlockCount = 1;
+            ItemID.Sets.CanGetPrefixes[Type] = false;
+            ItemID.Sets.ShimmerTransformToItem[Type] = 576;
+            MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, "Assets/Music/TheRealityoftheProphecy"), ModContent.ItemType<ProphecyMusicBox>(), ModContent.TileType<ProphecyMusicBoxTile>(), 0);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<ProphecyMusicBoxTile>();
+            Item.width = 32;
+            Item.height = 48;
+            Item.rare = ItemRarityID.Master;
+            Item.value = 100000;
+            Item.accessory = true;
+        }
+
+        public override void AddRecipes()
+        {
+            if (ModLoader.TryGetMod("FargowiltasSouls", out Mod souls))
+            {
+                CreateRecipe()
+                    .AddIngredient(souls.Find<ModItem>("MutantRelic"))
+                    .AddIngredient(ItemID.MusicBox)
+                    .AddTile(TileID.HeavyWorkBench)
+                    .Register();
+            }
+            else
+            {
+                CreateRecipe()
+                    .AddIngredient<DraedonRelic>()
+                    .AddIngredient<SupremeCalamitasRelic>()
+                    .AddIngredient(ItemID.Zenith)
+                    .AddIngredient(ItemID.MusicBox)
+                    .AddTile(TileID.HeavyWorkBench)
+                    .Register();
+            }
+        }
+    }
+}
